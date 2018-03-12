e8a1c73913853de28acdbdc59c88c6f28fbe87fb
imbrik <37290625+imbrik@users.noreply.github.com>
2018-03-12 14:12:41 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,11 @@
 curl -fsSL https://get.docker.com/ | sh
+sleep 30
 sudo systemctl start docker
+sleep 30
 sudo systemctl enable docker
+sleep 30
 sudo docker pull portainer/portainer:latest
+sleep 30
 sudo docker volume create --name portainer_data1
+sleep 30
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data1:/data portainer/portainer
