620ef99b5b4b9efd04efebd1660f93094658a5a7
imbrik <37290625+imbrik@users.noreply.github.com>
2018-03-12 13:26:13 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data1:/data portainer/portainer
