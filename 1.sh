e45160cac8478c7c5200972c6c47c3256ef72bca
imbrik <37290625+imbrik@users.noreply.github.com>
2018-03-12 13:57:15 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,4 @@
+curl -fsSL https://get.docker.com/ | sh
 sudo docker pull portainer/portainer:latest
 sudo docker volume create --name portainer_data1
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data1:/data portainer/portainer
